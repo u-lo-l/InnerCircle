80d72055d9a08ab791c2669c541dd79cd1e06cbe
u-lo-l <jeffk0408@naver.com>
2022-04-16 15:25:31 +0900

    philo

--- 3_circle/philo_practice/practice1.c
+++ 3_circle/philo_practice/practice1.c
@@ -5,45 +5,133 @@
 
 #define TRUE 1
 #define FALSE 0
+#define NOP 5
+#define NOE 3
+
+int	die = 0;
 
 typedef pthread_mutex_t	t_mutex;
 
 typedef struct	s_philo
 {
-	int	id;
-	int	eat_cnt;
+	int			id;
+	int			eat_count;
+	t_mutex		*left;
+	t_mutex		*right;
+	t_mutex		*eat_mutex;
+	t_mutex		*putlog_mutex;
+	pthread_t	philo_thread;
 }		t_philo;
 
 typedef struct	s_table
 {
 	int		num_of_philos;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
 	int		num_of_eats;
 	t_mutex	*fork_mutex_arr;
-	t_mutex	log_mutex;
 	t_mutex	eat_mutex;
+	t_mutex	putlog_mutex;
 	t_philo	*philo_arr;
 }			t_table;
 
-int	init_philosophers(t_table *table, int argc, char **argv)
+int	init_philos(t_table *table)
 {
-	if (!table)
-		return (FALSE);
-	table->num_of_philos = atoi(argv[1]);
-	table->time_to_die = atoi(argv[2]);
-	table->time_to_eat = atoi(argv[3]);
-	table->time_to_sleep = atoi(argv[4]]);
-	table->num_of_eats = 0;
-	if (argc == 6)
-		table->num_of_eats = atoi(argv[5]);
-	
+	int	i;
+
+	table->philo_arr = malloc(sizeof(t_philo) * table->num_of_philos);
+	table->fork_mutex_arr = malloc(sizeof(t_mutex) * table->num_of_philos);
+	i = -1;
+	while (++i < table->num_of_philos)
+	{
+		table->philo_arr[i].id = i + 1;
+		table->philo_arr[i].eat_count = table->num_of_eats;
+		if (pthread_mutex_init(&(table->fork_mutex_arr[i]), NULL) != 0)
+			return (FALSE);
+	}
+	pthread_mutex_init(&(table->eat_mutex), NULL);
+	pthread_mutex_init(&(table->putlog_mutex), NULL);
+	i = -1;
+	while (++i < table->num_of_philos)
+	{
+		table->philo_arr[i].left = &(table->fork_mutex_arr[i]);
+		table->philo_arr[i].right = &(table->fork_mutex_arr[(i + 1) % table->num_of_philos]);
+		table->philo_arr[i].eat_mutex = &(table->eat_mutex);
+		table->philo_arr[i].putlog_mutex = &(table->putlog_mutex);
+	}
+	return (TRUE);
 }
 
-int	main(int argc, char **argv)
+void	*start(void *vargp)
 {
-	if (argc != 5 && argc != 6)
-		return (EXIT_FAILURE);
-	
+	int		i;
+	t_philo	*philo;
+
+	philo = vargp;
+	i = 0;
+	while (!die)
+	{
+		printf("id %d left<%p> right<%p>\n", philo->id, philo->left, philo->right);
+		pthread_mutex_lock(philo->left);
+		printf("%d get left fork\n", philo->id);
+		usleep(100);
+		pthread_mutex_lock(philo->right);
+		printf("%d get right fork\n", philo->id);
+		usleep(100);
+		printf("%d starts eating : %d eats left\n", philo->id, philo->eat_count);
+		sleep(2);
+		printf("%d finished eating : %d eats left\n", philo->id, philo->eat_count);
+		philo->eat_count--;
+		if (philo->eat_count == 0)
+			die = 1;
+		pthread_mutex_unlock(philo->left);
+		pthread_mutex_unlock(philo->right);
+	}
+	return (NULL);
+}
+
+int	init_thread(t_table *table)
+{
+	int i;
+
+	i = -1;
+	while (++i < table->num_of_philos)
+	{
+		if (pthread_create(&(table->philo_arr[i].philo_thread), \
+							NULL, start, (void *)&(table->philo_arr[i])) != 0)
+			return (FALSE);
+		pthread_detach(table->philo_arr[i].philo_thread);
+		usleep(300);
+	}
+	return (TRUE);
+}
+
+int	end_thread(t_table *table)
+{
+	int	i;
+	i = -1;
+	while (++i < table->num_of_philos)
+		pthread_join(table->philo_arr[i].philo_thread, NULL);
+	return (TRUE);
+}
+
+int	clean_table(t_table *table)
+{
+	int i;
+	i = -1;
+	while (++i < table->num_of_philos)
+		pthread_mutex_destroy(&(table->fork_mutex_arr[i]));
+	free(table->philo_arr);
+	free(table->fork_mutex_arr);
+	return (TRUE);
+}
+
+int main()
+{
+	t_table table;
+	table.num_of_philos = NOP;
+	table.num_of_eats = NOE;
+
+	if (init_philos(&table) == FALSE)	return (FALSE);
+	if (init_thread(&table) == FALSE)	return (FALSE);
+	end_thread(&table);
+	return (clean_table(&table));
 }
---
+++ 3_circle/philo_practice/practice2.c
@@ -0,0 +1,88 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef pthread_mutex_t	t_mutex;
+
+struct table;
+
+typedef struct temp
+{
+	int			id;
+	int			count;
+	t_mutex		*left;
+	t_mutex		*right;
+	pthread_t	thread;
+	struct table *tab;
+}		t_temp;
+
+typedef struct table
+{
+	int		total_num;
+	int		total_cnt;
+	t_mutex	*mutex_arr;
+	t_temp	*temp_arr;
+}			t_table;
+
+t_mutex	mut;
+
+void	*two_mut_func(void *vargp)
+{
+	t_temp	*temp = vargp;
+
+	pthread_mutex_lock(&mut);
+	printf("=====VARG INFO=====\n");
+	printf("ID : %d\n", temp->id);
+	printf("L  : %p\n", temp->left);
+	printf("R  : %p\n", temp->right);
+	printf("===================\n");
+	pthread_mutex_unlock(&mut);
+
+	pthread_mutex_lock(temp->left);
+	printf("%d locked left\n", temp->id);
+	pthread_mutex_lock(temp->right);
+	printf("%d locked right\n", temp->id);
+	pthread_mutex_lock(&mut);
+	printf("%d wait for 1sec\n", temp->id);
+	int k;
+	printf("@@@@@@\n");
+	for (k = 0 ; k < temp->tab->total_cnt ; k++)
+	{
+		printf("%d->%d\n", temp->id, k + 1);
+		usleep(1000000 / temp->tab->total_cnt);
+	}
+	printf("@@@@@@\n");
+	pthread_mutex_unlock(&mut);
+	pthread_mutex_unlock(temp->left);
+	pthread_mutex_unlock(temp->right);
+	printf("%d unlock\n\n", temp->id);
+}
+
+int main()
+{
+	t_table	table;
+
+	table.total_num = 5;
+	table.total_cnt	= 3;
+	table.mutex_arr = malloc(sizeof(t_mutex) * table.total_num);
+	table.temp_arr = malloc(sizeof(t_temp) * table.total_num);
+	
+	pthread_mutex_init(&mut, NULL);
+	for (int i = 0 ; i < table.total_num ; i++)
+		pthread_mutex_init(table.mutex_arr + i , NULL);
+	for (int i = 0 ; i < table.total_num ; i++)
+	{
+		table.temp_arr[i].id = i + 1;
+		table.temp_arr[i].tab = &table;
+		table.temp_arr[i].left = &table.mutex_arr[i];
+		table.temp_arr[i].right = &table.mutex_arr[(i + 1) % table.total_num];
+		pthread_create(&(table.temp_arr[i].thread), NULL, two_mut_func, &(table.temp_arr[i]));
+		usleep(1);
+	}
+	for (int i = 0 ; i < table.total_num ; i++)
+		pthread_join(table.temp_arr[i].thread, NULL);
+	for (int i = 0 ; i < table.total_num; i++)
+		pthread_mutex_destroy(table.mutex_arr + i);
+	return (0);
+}
