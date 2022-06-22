6cf25e6090e3e7f2c6cfa950f38cc5b61b273d25
ulol <jeffk0408@naver.com>
2022-06-22 15:30:09 +0900

    cpp00 ex00

---
+++ 4_circle/cpp_modules/CPP00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+// NO STL(vector map lst ,,,), NO <algorithm>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1 ; i < argc ; i++)
+		{
+			std::string str(argv[i]);
+			for (unsigned int j = 0; j < str.length(); j++)
+				std::cout << char (std::toupper(str[j]));
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
