56ee9849542ee3a9a680948b34f2df19d62b16dd
u-lo-l <jeffk0408@naver.com>
2022-07-02 22:51:38 +0900

    fix bsp

--- 4_circle/cpp_modules/CPP02/ex03/bsp.cpp
+++ 4_circle/cpp_modules/CPP02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 18:19:36 by dkim2             #+#    #+#             */
-/*   Updated: 2022/07/02 21:22:49 by dkim2            ###   ########.fr       */
+/*   Updated: 2022/07/02 22:49:54 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ bool	bsp(Point const a, Point const b, Point const c,
 	vecA = b - a;
 	vecB = c - a;
 	d = vecA.getX() * vecB.getY() - vecA.getY() * vecB.getX();
-	u = p.getX() / d;
-	v = p.getY() / d;
+	u = p.getX() * vecB.getY() - p.getY() * vecB.getX();
+	v = p.getY() * vecA.getX() - p.getX() * vecA.getY();
+	u = u / d;
+	v = v / d;
 	if (Fixed(0) < u && u < Fixed(1) && \
 		Fixed(0) < v && v < Fixed(1) && \
 		u + v < Fixed(1))
