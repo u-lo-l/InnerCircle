d6e02608b9539f55f7c9947674ef5663c648be20
ulol <jeffk0408@naver.com>
2022-07-23 17:53:51 +0900

    CPP08

--- 4_circle/cpp_modules/CPP08/ex01/Span.hpp
+++ 4_circle/cpp_modules/CPP08/ex01/Span.hpp
@@ -3,45 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkim2 <dkim2@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 15:14:32 by dkim2             #+#    #+#             */
-/*   Updated: 2022/07/20 18:16:57 by dkim2            ###   ########.fr       */
+/*   Updated: 2022/07/23 16:28:29 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 # define SPAN_HPP
-# include <vector>
+# include <set>
 # include <exception>
 
 class Span
 {
 private:
-    unsigned _size;
-    const unsigned int _capacity;
-    std::vector<int> _arr;
-    Span( void );
+	const unsigned int _capacity;
+	unsigned int _size;
+	std::multiset<int> _arr;
 public:
-    Span( unsigned int N );
-    ~Span();
-    Span( const Span & s);
-    Span & operator=( const Span & s);
+	Span( unsigned int N = 0);
+	~Span();
+	Span( const Span & s);
+	Span & operator=( const Span & s);
 
-    struct tooManyElements : public std::exception
-    { const char * what() const throw(); };
-    struct notEnoughElemnts : public std::exception
-    { const char * what() const throw(); };
+	struct tooManyElements : public std::exception
+	{ const char * what() const throw(); };
+	struct notEnoughElemnts : public std::exception
+	{ const char * what() const throw(); };
 
-    void addNumber( const int & a );
-    template <typename Iterator>
-    void addNumber( Iterator begin, Iterator end);
+	void addNumber( int a );
+	template <typename Iterator>
+	void addNumber( Iterator begin, Iterator end);
 
-    unsigned int shortestSpan( void ) const;
-    unsigned int longestSpan( void ) const;
-    
-    unsigned int getSize( void ) const;
-    unsigned int getCapacity( void ) const;
+	unsigned int shortestSpan( void ) const;
+	unsigned int longestSpan( void ) const;
+	void showInfo( void ) const;
+
+	unsigned int getSize( void ) const;
+	unsigned int getCapacity( void ) const;
 };
 
 // 들어온 iterator를 이용해서 추가되는 원소의 개수(n)을 확인하고
@@ -49,13 +49,8 @@ public:
 template <typename Iterator>
 void Span::addNumber(Iterator begin, Iterator end)
 {
-    unsigned int n = 0;
-    for (Iterator i = begin ; i != end ; i++)
-        n++;
-    if (_arr.size() + n > _capacity)
-        throw (tooManyElements());
-    _arr.insert(_arr.end(), begin, end);
-    _size += n;
+	for (Iterator i = begin ; i != end ; i++)
+		this->addNumber(static_cast<int>(*i));
 }
 
 
